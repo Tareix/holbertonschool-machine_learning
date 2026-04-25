240033ed5d6c7c6b39d9a668c993468c59208e29
Tareix <cahidmh30@gmail.com>
2026-04-25 12:39:04 +0000

    D

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Adds two 2D matrices element-wise"""
+
+
+def add_matrices2D(mat1, mat2):
+    """Function that adds two 2D matrices element-wise"""
+    if len(mat1) != len(mat2):
+        return None
+
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+    return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
+            for i in range(len(mat1))]
