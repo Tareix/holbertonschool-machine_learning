22fa3ef30d4265180636f1e9df658bfca769433b
Tareix <cahidmh30@gmail.com>
2026-05-09 11:57:48 +0200

    d

---
+++ math/probability/normal.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""Contains the Normal class to represent a normal distribution"""
+
+
+class Normal:
+    """Represents a normal distribution"""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """Initializes the normal distribution"""
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = float(sum(data) / len(data))
+            sum_diff_sq = sum((x - self.mean) ** 2 for x in data)
+            self.stddev = float((sum_diff_sq / len(data)) ** 0.5)
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value"""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score"""
+        return self.mean + (z * self.stddev)
+
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given x-value"""
+        pi = 3.1415926536
+        e = 2.7182818285
+        exponent = -0.5 * (self.z_score(x) ** 2)
+        coefficient = 1 / (self.stddev * ((2 * pi) ** 0.5))
+        return coefficient * (e ** exponent)
+
+    def cdf(self, x):
+        """Calculates the value of the CDF for a given x-value"""
+        pi = 3.1415926536
+        # erf argument: (x - mean) / (stddev * sqrt(2))
+        val = (x - self.mean) / (self.stddev * (2 ** 0.5))
+
+        # Error function approximation
+        erf = (2 / (pi ** 0.5)) * (
+            val - (val ** 3) / 3 + (val ** 5) / 10 -
+            (val ** 7) / 42 + (val ** 9) / 216
+        )
+
+        return 0.5 * (1 + erf)
