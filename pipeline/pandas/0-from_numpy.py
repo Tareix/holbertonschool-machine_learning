a488189cf43bfc89f8c37a0ac1825ffa76a6deac
Tareix <cahidmh30@gmail.com>
2026-05-29 12:36:30 +0200

    D

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+"""Module for creating a Pandas DataFrame from a NumPy array."""
 
 import pandas as pd
 
-def from_numpy(array):
 
-    
+def from_numpy(array):
+    """Create a DataFrame from a NumPy array."""
     num_cols = array.shape[1]
     columns = [chr(65 + i) for i in range(num_cols)]
     return pd.DataFrame(array, columns=columns)
