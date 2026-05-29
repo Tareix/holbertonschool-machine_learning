a3f4583ef9de050e3b2674215876bf3e183a44dd
Tareix <cahidmh30@gmail.com>
2026-05-29 13:12:02 +0200

    D

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Module for slicing a DataFrame."""
 
-def slice(df):
 
-    
+def slice(df):
     """Select specific columns and every 60th row."""
     columns = ['High', 'Low', 'Close', 'Volume_(BTC)']
     return df[columns].iloc[::60]
