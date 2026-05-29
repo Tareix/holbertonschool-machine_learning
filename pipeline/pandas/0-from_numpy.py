31f7f43f0f95ecb5cc89593abacea14345fcaaf7
Tareix <cahidmh30@gmail.com>
2026-05-29 12:31:28 +0200

    D

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 def from_numpy(array):
     
-    num_cols = array.shape(1)
+    num_cols = array.shape[1]
     columns = [chr(65 + i) for i in range(num_cols)]
     return pd.DataFrame(array, columns=columns)
