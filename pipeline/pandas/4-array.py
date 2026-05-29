c91e940cecccafaea05a66c3047d7fc562c16fd5
Tareix <cahidmh30@gmail.com>
2026-05-29 13:03:23 +0200

    D

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """Module for converting DataFrame columns to a NumPy array."""
 
-import pandas as pd
-
 
 def array(df):
     """Return the last 10 rows of High and Close as a NumPy array."""
